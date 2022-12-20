9ffeb894311495f61230990de8f7a749af319252
pattharajarin_p@cmu.ac.th <pattharajarin_p@cmu.ac.th>
2022-12-20 15:04:35 +0700

    ok

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a  =  20, b = 30;
+    int c = 4;
+    float output ,d;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    d=(float)b/c ;
+    output = (float)a - d;
+    cout << "output: "<< output ;
     return 0;
 }
